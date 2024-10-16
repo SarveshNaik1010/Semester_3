03b51daf0ad4c06157cfbd719eb7bb21fa3b2ea0
Sarvesh Naik <naiksarvesh463@gmail.com>
2024-10-16 23:16:58 +0530

    FDS Project 1

--- Python/Python-Project/app.py
+++ Python/Python-Project/app.py
@@ -1,6 +1,66 @@
-import streamlit as st
+from flask import Flask, render_template, request, jsonify;
+import pymongo
+from pymongo import ReturnDocument
+import json
+
+# DB Setup
+client = pymongo.MongoClient('localhost:27017');
+db = client['library']
+collection = db['users']
+
+# Creating the server
+app = Flask(__name__)
+
+def sendResponse(status, message="", documents=None):
+    if(documents == None): return {"status": status, "message": message }
+    return {"status": status, "message": message, "data": documents };
 
 
-st.title("Welcome to Library")
-st.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia n")
-st.link_button("Login", "/login", type="secondary")
+
+
+# Frontend
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/library")
+def library():
+    return render_template('library.html')
+
+
+# Backend
+@app.route('/api/v1/user', methods=['GET', 'POST'])
+def manageUsers():
+    if(request.method == 'POST'):
+        body = request.get_json();
+        cursor = collection.find();
+        for document in cursor:
+            if(document['username'] == body['username']):
+                return jsonify({"status": "error", "message": f"username '{body['username']}' is already taken!"})
+        collection.insert_one(body);
+        return (jsonify({"status": "success", "message": "User created successfully"}))
+    if(request.method == 'GET'):
+        cursor = collection.find();
+        users = [];
+        for document in cursor:
+            document['_id'] = str(document['_id']);
+            users.append(document)
+        print(users)
+        return users;
+
+@app.route('/api/v1/user/<string:name>', methods=['PATCH'])
+def updateUser(name):
+    body = request.get_json()
+    updatedUser = collection.find_one_and_update({'username': name}, {"$set": body}, return_document=ReturnDocument.AFTER)
+    if(updatedUser == None): return sendResponse("error", "User does not exists")
+    updatedUser['_id'] = str(updatedUser['_id']);
+    return sendResponse('success', "User updated succesfully", updatedUser);
+
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)  # Enable debug mode
--- Python/Python-Project/newfile.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-app = Flask("newfile")
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello,    sdf!</p>"
