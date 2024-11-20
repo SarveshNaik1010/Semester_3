07a8aec3a5f0cef3ff370c84db5f910dba09ee5c
Sarvesh Naik <naiksarvesh463@gmail.com>
2024-11-20 22:35:26 +0530

    DSA Practice

--- Data Structure/practice.c
+++ Data Structure/practice.c
@@ -1,63 +1,66 @@
 #include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
+#include <stdlib.h>
 
-#define MAX_STUDENTS 12
+struct List{
+    int data;
+    struct List* next;    
+};
 
-// Structure to represent each student and their subject
-typedef struct {
-    int id;
-    char subject[20];
-    bool isPaired;
-} Student;
+struct List* pushNode(struct List* Head, int data) {
+    // struct List* newNode = (struct List*)malloc(sizeof(struct List));
+    struct List* newNode = (struct List*)malloc(sizeof(struct List));
+    newNode->data = data;
+    newNode->next = Head == NULL ? NULL : Head;
+    struct List* currNode = Head;
 
-// Function to display pairs
-void displayPairs(Student students[], int pairs[][2], int pairCount) {
-    printf("Student Pairs:\n");
-    for (int i = 0; i < pairCount; i++) {
-        printf("Student %d (%s) --- Student %d (%s)\n",
-               students[pairs[i][0]].id, students[pairs[i][0]].subject,
-               students[pairs[i][1]].id, students[pairs[i][1]].subject);
-    }
+    if(Head == NULL) return newNode;
+
+    currNode->next = newNode;
+
+    printf("Element %d added to the list\n", newNode->data);
+
+    return Head;
 }
 
-// Greedy function to pair students with different subjects
-void pairStudents(Student students[], int numStudents) {
-    int pairs[MAX_STUDENTS / 2][2]; // Array to store paired student indices
-    int pairCount = 0;
+struct List* insertAtBegning(struct List* Head, int data) {
+    struct List* currNode = (struct List*)malloc(sizeof(struct List));
+    currNode->data = data;
+    currNode->next = Head;
+    Head = currNode;
+    printf("%d\n", Head->data);
+    return Head;
+}
 
-    for (int i = 0; i < numStudents; i++) {
-        if (!students[i].isPaired) { // If the student isn't paired yet
-            for (int j = i + 1; j < numStudents; j++) {
-                // Check if student is unpaired and has a different subject
-                if (!students[j].isPaired && 
-                    students[i].id != students[j].id && 
-                    strcmp(students[i].subject, students[j].subject) != 0) {
-                    
-                    pairs[pairCount][0] = i; // Pair student i
-                    pairs[pairCount][1] = j; // with student j
-                    students[i].isPaired = true;
-                    students[j].isPaired = true;
-                    pairCount++;
-                    break; // Move to the next student to pair
-                }
+struct List* display(struct List* Head) {
+    struct List* currNode = Head;
+    while(currNode != NULL) {
+        printf("Data - %d\n", currNode->data);
+        currNode = currNode->next;
+    }
+    return Head;
+}
+
+struct List* pop(struct List* Head) {
+    while(1) {
+        struct List* nextNode = Head;
+        while(1) {
+            if(nextNode->next->next == NULL) {
+                nextNode->next = NULL;
+                break;
             }
+            nextNode = nextNode->next;
         }
+        printf("Element Popped\n");
+        return Head;
     }
-
-    displayPairs(students, pairs, pairCount);
 }
 
-int main() {
-    Student students[MAX_STUDENTS] = {
-        {1, "Math", false}, {2, "Math", false}, {3, "Science", false}, 
-        {4, "Science", false}, {5, "History", false}, {6, "English", false},
-        {7, "Geography", false}, {8, "Math", false}, {9, "Science", false},
-        {10, "History", false}, {11, "Geography", false}, {12, "English", false}
-    };
-
-    printf("Pairing students based on different subjects using a greedy approach:\n");
-    pairStudents(students, MAX_STUDENTS);
-
-    return 0;
+void main() {
+    struct List* Head = pushNode(NULL, 10);
+    pushNode(Head, 10);
+    pushNode(Head, 20);
+    pushNode(Head, 30);
+    // Head = insertAtBegning(Head, 40);
+    Head = pop(Head);
+    display(Head);
 }
